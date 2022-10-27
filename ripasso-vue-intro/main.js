8724338865ffc6a17e104f929d198dacdd3218a1
Francesca-Barcella <pollon.fra@gmail>
2022-10-27 08:36:39 +0200

    change color  con v-on:click

--- ripasso-vue-intro/main.js
+++ ripasso-vue-intro/main.js
@@ -4,12 +4,35 @@ createApp({
     data() {
 
         return {
-            //message: 'hello vue!',
+            message: 'hello vue!',
             title: 'Ripasso vue js',
-            isButtonDisabled: true, // DISABILITATO
-            //isButtonDisabled: false, // ABILITATO
+            //isButtonDisabled: true, // DISABILITATO
+            isButtonDisabled: false, // ABILITATO
             //isButtonDisabled: '', // STRINGA VUOTA === false == ABILITATO
+            number: 3,
+            userName: '',
+            textColor: 'color',
 
+            //gestione attributi di un tag
+            attributiDiv: {
+                id: 'bgColor',
+                //se gli do class container ed ho installato bootsrap prende già le impostazioni di bootstrap
+                class: 'container',
+            },
+        }
+    },
+
+    methods: {
+        changeColor: function () {
+            console.log('hai cliccato cambia colore');
+            // con il this recupera textColor perchè il methods che lo usa è scritto all'ìinterno del blocco createApp
+            if (this.textColor === 'color') {
+                this.textColor = 'newColor';
+            } else {
+                this.textColor = 'color';
+            }
+        
         }
     }
-}).mount('#app');
+
+    }).mount('#app');
