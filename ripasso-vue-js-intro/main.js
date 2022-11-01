e1f8c54304beaea2b3bf273ae25893b0ebf53e1a
Francesca-Barcella <pollon.fra@gmail>
2022-11-01 11:53:19 +0100

    Vue H^JS - classi e stili come oggetto in istanza

--- ripasso-vue-js-intro/main.js
+++ ripasso-vue-js-intro/main.js
@@ -12,28 +12,34 @@ createApp({
             number: 3,
             userName: '',
             textColor: 'color',
-            isActive: true,
-            hasError: true,
 
+            isActive: true,
+            hasError: false,
+           
+           
+            classObject:{
+                active: true,
+                'text-primary': true
+            },
 
             //gestione attributi di un tag
             attributiDiv: {
                 id: 'bgColor',
-                //se gli do class container ed ho installato bootsrap prende già le impostazioni di bootstrap
+                    //se gli do class container ed ho installato bootsrap prende già le impostazioni di bootstrap
                 class: 'container',
-            },
-        }
-    },
+                },
+    }
+},
 
     methods: {
-        changeColor: function () {
-            console.log('hai cliccato cambia colore');
-            // con il this recupera textColor perchè il methods che lo usa è scritto all'ìinterno del blocco createApp
-            if (this.textColor === 'color') {
-                this.textColor = 'newColor';
-            } else {
-                this.textColor = 'color';
-            }
+    changeColor: function () {
+        console.log('hai cliccato cambia colore');
+        // con il this recupera textColor perchè il methods che lo usa è scritto all'ìinterno del blocco createApp
+        if (this.textColor === 'color') {
+            this.textColor = 'newColor';
+        } else {
+            this.textColor = 'color';
         }
     }
+}
 }).mount('#app');
