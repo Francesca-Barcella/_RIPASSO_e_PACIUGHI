71cf957ecd8d349f6a58881f267bebd49b0eb3f0
Francesca-Barcella <pollon.fra@gmail>
2022-11-07 14:52:11 +0100

    separato da ripasso array

--- ripasso-js__forEach-map-filter/main.js
+++ ripasso-js__forEach-map-filter/main.js
@@ -1,32 +1,15 @@
 //#region CICLO FOR EACH
 
-//alert('funziono')
-
+//for each su array
 const words = ['ciao', 'weekend lungo', 'addio'];
 console.log(words);
-//aggiunge un alemento alla fine
-words.push('bentornato');
-words.push('buon viaggio');
-words.push('buone vacanze');
-words.push('arrivederci');
-//viene aggiunto un alemento all'inizio dell'array
-words.unshift('come va?')
-words.unshift('tutto bene?')
 
-//rimuove l'ultimo elemento dell'array
-words.pop(); // toglie arrivederci
-//rimuove il primo elemento dell'arraya
-words.shift(); // toglie tutto bene?
-//mette in ordine alfabetico/numerico crescente
-words.sort();
-//trovare l'ultimo elemenento dell'array senza specificare il numero dell'indice
-let word = words[words.length - 1]
-console.log('log dell\'ultima parola dell\'array = ' + word);
 
 words.forEach((element, index, words_array) => {
     console.log(element, index);
 });
 
+//for each su oggetto
 const objects = [
     {
         name: 'bal',
