f7b86e9ed7382e1e36a7d01e84e9cb47798c370b
Francesca-Barcella <pollon.fra@gmail>
2022-11-07 14:45:18 +0100

    .legnht - 1

--- ripasso-js__forEach-map-filter/main.js
+++ ripasso-js__forEach-map-filter/main.js
@@ -2,19 +2,26 @@
 
 //alert('funziono')
 
-const words = ['ciao', 'weekddìen lungo', 'addio'];
+const words = ['ciao', 'weekend lungo', 'addio'];
 console.log(words);
+//aggiunge un alemento alla fine
 words.push('bentornato');
 words.push('buon viaggio');
 words.push('buone vacanze');
 words.push('arrivederci');
+//viene aggiunto un alemento all'inizio dell'array
+words.unshift('come va?')
+words.unshift('tutto bene?')
 
 //rimuove l'ultimo elemento dell'array
-//words.pop();
+words.pop(); // toglie arrivederci
 //rimuove il primo elemento dell'arraya
-words.shift();
+words.shift(); // toglie tutto bene?
 //mette in ordine alfabetico/numerico crescente
 words.sort();
+//trovare l'ultimo elemenento dell'array senza specificare il numero dell'indice
+let word = words[words.length - 1]
+console.log('log dell\'ultima parola dell\'array = ' + word);
 
 words.forEach((element, index, words_array) => {
     console.log(element, index);
